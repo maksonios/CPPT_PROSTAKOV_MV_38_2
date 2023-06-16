e30c5c1850b7ada031c659d23aeb7da1abb47a75
Maksym Prostakov <maksymprostakov@gmail.com>
2023-06-17 00:42:14 +0300

    Minor js refactoring

--- src/EncryptionUtility/wwwroot/js/site.js
+++ src/EncryptionUtility/wwwroot/js/site.js
@@ -1,11 +1,11 @@
 ﻿var currentUrl = window.location.pathname;
 var segments = currentUrl.split('/');
 var controllerName = segments[1];
-var dropzone = new Dropzone("#my-dropzone", { 
+
+new Dropzone("#my-dropzone", { 
     url: '/' + controllerName + '/upload',
     maxFiles: 1,
     sending: (file, xhr, formData) => {
-        $("#algorithm").val();
         formData.append("algorithm", $("#algorithm").val());
     },
     success: (response) => {
