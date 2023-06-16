fe64a286562fc0bd42047f2f75dfc81a4ef31646
Maksym Prostakov <maksymprostakov@gmail.com>
2023-06-17 00:54:02 +0300

    Minor dropzone.js fixes

--- src/EncryptionUtility/wwwroot/js/site.js
+++ src/EncryptionUtility/wwwroot/js/site.js
@@ -2,9 +2,16 @@
 var segments = currentUrl.split('/');
 var controllerName = segments[1];
 
-new Dropzone("#my-dropzone", { 
+var dropzone = new Dropzone("#my-dropzone", { 
     url: '/' + controllerName + '/upload',
     maxFiles: 1,
+    maxFilesize: 3,
+    autoProcessQueue: false,
+    multiple: false,
+    maxfilesexceeded: function (files) {
+        this.removeAllFiles();
+        this.addFile(files);
+    },
     sending: (file, xhr, formData) => {
         formData.append("algorithm", $("#algorithm").val());
     },
@@ -12,4 +19,9 @@ new Dropzone("#my-dropzone", {
         var result = response.xhr.response.substring(1, response.xhr.response.length-1);
         $("#result").val(result);
     }
+});
+
+$("#submit").click(function (e) {
+    e.preventDefault();
+    dropzone.processQueue();
 });
