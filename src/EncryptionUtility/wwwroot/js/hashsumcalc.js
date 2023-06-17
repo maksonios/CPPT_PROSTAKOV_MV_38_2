b3e40d6a2e7202d4c48b75c10e93f516291a9de8
Maksym Prostakov <maksymprostakov@gmail.com>
2023-06-17 22:29:48 +0300

    Refactor URL defining

---
+++ src/EncryptionUtility/wwwroot/js/hashsumcalc.js
@@ -0,0 +1,23 @@
+var dropzone = new Dropzone("#my-dropzone", {
+    url: '/HashsumCalc/Upload',
+    maxFiles: 1,
+    maxFilesize: 3,
+    autoProcessQueue: false,
+    multiple: false,
+    maxfilesexceeded: function (files) {
+        this.removeAllFiles();
+        this.addFile(files);
+    },
+    sending: (file, xhr, formData) => {
+        formData.append("algorithm", $("#algorithm").val());
+    },
+    success: (response) => {
+        var result = response.xhr.response.substring(1, response.xhr.response.length-1);
+        $("#result").val(result);
+    }
+});
+
+$("#submit").click(function (e) {
+    e.preventDefault();
+    dropzone.processQueue();
+});
